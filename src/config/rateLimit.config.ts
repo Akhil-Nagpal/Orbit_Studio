59079b7d5c08db8741d526926f3179481cb16847
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-05-31 01:23:43 +0530

    fix: env based rate limits

--- src/config/rateLimit.config.ts
+++ src/config/rateLimit.config.ts
@@ -2,25 +2,27 @@
 
 // This configuration is for tuning time limits for specific routes
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const rateLimitConfig = {
   api: {
     windowMs: 15 * 60 * 1000, // 15 minutes time window in milliseconds
-    limit: 300, // added limit withing 15 min's of window
+    limit: isProduction ? 300 : 100000, // added limit withing 15 min's of window
   },
   auth: {
     windowMs: 15 * 60 * 1000, // 10 minutes time window
-    limit: 10, // added limit for auth
+    limit: isProduction ? 10 : 1000, // added limit for auth
   },
   upload: {
     windowMs: 60 * 60 * 1000, // 60 minutes time window
-    limit: 20, // limit for uploading videos
+    limit: isProduction ? 20 : 1000, // limit for uploading videos
   },
   comment: {
     windowMs: 10 * 60 * 1000, // 10 minutes time window
-    limit: 30, // limit for adding comments or updating comments
+    limit: isProduction ? 30 : 1000, // limit for adding comments or updating comments
   },
   view: {
     windowMs: 1 * 60 * 1000, // 1 minute time window
-    limit: 50, // limit for adding view in 1 minute
+    limit: isProduction ? 50 : 10000, // limit for adding view in 1 minute
   },
 };
